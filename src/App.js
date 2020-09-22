79dfa1bc9aec506d86e3f764bd4dba9e09af6d64
Adilson Jr <adilsondjr@gmail.com>
2020-09-22 20:29:47 -0300

    Desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepo] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepo(res.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const reponse = await api.post('repositories', {
+      title: 'Web',
+      url: 'http://github.com/adilsondjr/web',
+      techs: ['HTML', 'Javascript', 'CSS']
+    })
+
+    setRepo([ ...repos, reponse.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepo(repos.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map(repo => (
+          <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
